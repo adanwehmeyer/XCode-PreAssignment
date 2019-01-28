972ae5a5ffb0c4ef12ef13dc15e390bb91f99c64
Adan Wehmeyer <adanwehmeyer@adans-macbook-air.local>
2019-01-28 14:55:07 -0600

    Initial Commit

---
+++ TipppsyTests/TipppsyTests.swift
@@ -0,0 +1,34 @@
+//
+//  TipppsyTests.swift
+//  TipppsyTests
+//
+//  Created by Adan Wehmeyer on 1/28/19.
+//  Copyright © 2019 Adan Wehmeyer. All rights reserved.
+//
+
+import XCTest
+@testable import Tipppsy
+
+class TipppsyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
